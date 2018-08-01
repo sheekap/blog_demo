0a3846b71b8cdddb64cd218584dcc3aac28ddcdd
Sheeka Patak <spatak@zendesk.com>
2018-08-01 13:21:23 +0100

    fix incorrect name of foreign key columns

--- db/migrate/20180729192930_update_foreign_keys_on_posts_and_users.rb
+++ db/migrate/20180729192930_update_foreign_keys_on_posts_and_users.rb
@@ -1,12 +1,12 @@
 class UpdateForeignKeysOnPostsAndUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :posts, :user_id
+    remove_foreign_key :posts, :index_posts_on_user_id
     add_foreign_key :posts, :user_id, on_delete: :cascade
 
-    remove_foreign_key :comments, :post_id
+    remove_foreign_key :comments, :index_comments_on_post_id
     add_foreign_key :comments, :post_id, on_delete: :cascade
 
-    remove_foreign_key :comments, :user_id
+    remove_foreign_key :comments, :index_comments_on_user_id
     add_foreign_key :comments, :user_id, on_delete: :cascade
   end
 end
