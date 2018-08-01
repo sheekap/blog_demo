664137dd8ffd505ccddad694361c9a2ea015a25b
Sheeka Patak <spatak@zendesk.com>
2018-08-01 13:28:35 +0100

    rollback and delete migration

--- db/migrate/20180729192930_update_foreign_keys_on_posts_and_users.rb
+++
@@ -1,9 +0,0 @@
-class UpdateForeignKeysOnPostsAndUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_foreign_key :posts, :user_id, on_delete: :cascade
-
-    add_foreign_key :comments, :post_id, on_delete: :cascade
-
-    add_foreign_key :comments, :user_id, on_delete: :cascade
-  end
-end
